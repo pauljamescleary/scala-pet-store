53901c371a2d54ae68559b6267234bb54734a8bc
tbrown1979 <tbrown@uni.edu>
2017-12-18 22:21:47 -0600

    Addressing issue 39. No unsafeRunSync.

--- src/main/scala/io/github/pauljamescleary/petstore/Server.scala
+++ src/main/scala/io/github/pauljamescleary/petstore/Server.scala
@@ -18,26 +18,23 @@ import org.http4s.util.ExitCode
 object Server extends StreamApp[IO] {
 
   override def stream(args: List[String], shutdown: IO[Unit]): Stream[IO, ExitCode] =
-    createStream[IO](args, shutdown).unsafeRunSync()
+    createStream[IO](args, shutdown)
 
   def createStream[F[_]](args: List[String], shutdown: F[Unit])(
-      implicit E: Effect[F]): F[Stream[F, ExitCode]] =
+      implicit E: Effect[F]): Stream[F, ExitCode] =
     for {
-      conf <- PetStoreConfig.load[F]
-      xa <- DatabaseConfig.dbTransactor(conf.db)
-      _ <- DatabaseConfig.initializeDb(conf.db, xa)
-      petRepo = DoobiePetRepositoryInterpreter[F](xa)
-      orderRepo = DoobieOrderRepositoryInterpreter[F](xa)
-    } yield {
-
-      val petValidation = PetValidationInterpreter[F](petRepo)
-      val petService = PetService[F](petRepo, petValidation)
-      val orderService = OrderService[F](orderRepo)
-
-      BlazeBuilder[F]
+      conf          <- Stream.eval(PetStoreConfig.load[F])
+      xa            <- Stream.eval(DatabaseConfig.dbTransactor(conf.db))
+      _             <- Stream.eval(DatabaseConfig.initializeDb(conf.db, xa))
+      petRepo       =  DoobiePetRepositoryInterpreter[F](xa)
+      orderRepo     =  DoobieOrderRepositoryInterpreter[F](xa)
+      petValidation =  PetValidationInterpreter[F](petRepo)
+      petService    =  PetService[F](petRepo, petValidation)
+      orderService  =  OrderService[F](orderRepo)
+      exitCode      <- BlazeBuilder[F]
         .bindHttp(8080, "localhost")
         .mountService(PetEndpoints.endpoints[F](petService), "/")
         .mountService(OrderEndpoints.endpoints[F](orderService), "/")
         .serve
-    }
+    } yield exitCode
 }
