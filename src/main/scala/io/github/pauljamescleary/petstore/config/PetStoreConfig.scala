7d8d953e04c98b037a283f9b369b536812bdf70d
Dmitry Kostyanoy <dmitry.kostyanoy@gmail.com>
2018-01-08 11:12:04 +0100

    Don't throw exceptions when reading config fails

--- src/main/scala/io/github/pauljamescleary/petstore/config/PetStoreConfig.scala
+++ src/main/scala/io/github/pauljamescleary/petstore/config/PetStoreConfig.scala
@@ -1,6 +1,7 @@
 package io.github.pauljamescleary.petstore.config
 
 import cats.effect.Effect
+import cats.implicits._
 import pureconfig.error.ConfigReaderException
 
 case class PetStoreConfig(db: DatabaseConfig)
@@ -14,10 +15,8 @@ object PetStoreConfig {
     * return an error.  This should halt the application from starting up.
     */
   def load[F[_]](implicit E: Effect[F]): F[PetStoreConfig] =
-    E.delay {
-      loadConfig[PetStoreConfig]("petstore") match {
-        case Right(ok) => ok
-        case Left(e) => throw new ConfigReaderException[PetStoreConfig](e)
-      }
+    E.delay(loadConfig[PetStoreConfig]("petstore")).flatMap {
+      case Right(ok) => E.pure(ok)
+      case Left(e) => E.raiseError(new ConfigReaderException[PetStoreConfig](e))
     }
 }
