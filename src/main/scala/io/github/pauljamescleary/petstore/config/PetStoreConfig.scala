3364d9fbeeef80edd4bedec87d2f467e9fe923e2
Zach Abbott <zachary.abbott@banno.com>
2018-04-08 11:21:52 -0500

    Use Sync instead of Effect as it's the least powerful

--- src/main/scala/io/github/pauljamescleary/petstore/config/PetStoreConfig.scala
+++ src/main/scala/io/github/pauljamescleary/petstore/config/PetStoreConfig.scala
@@ -1,6 +1,6 @@
 package io.github.pauljamescleary.petstore.config
 
-import cats.effect.Effect
+import cats.effect.Sync
 import cats.implicits._
 import pureconfig.error.ConfigReaderException
 
@@ -14,7 +14,7 @@ object PetStoreConfig {
     * Loads the pet store config using PureConfig.  If configuration is invalid we will
     * return an error.  This should halt the application from starting up.
     */
-  def load[F[_]](implicit E: Effect[F]): F[PetStoreConfig] =
+  def load[F[_]](implicit E: Sync[F]): F[PetStoreConfig] =
     E.delay(loadConfig[PetStoreConfig]("petstore")).flatMap {
       case Right(ok) => E.pure(ok)
       case Left(e) => E.raiseError(new ConfigReaderException[PetStoreConfig](e))
