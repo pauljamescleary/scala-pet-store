3727d7b689810cc76adc673a52e33580b8ae8923
Zak Patterson <pattersonzak@gmail.com>
2019-01-16 13:09:38 -0500

    Fix error handling of config loading

--- src/main/scala/io/github/pauljamescleary/petstore/config/package.scala
+++ src/main/scala/io/github/pauljamescleary/petstore/config/package.scala
@@ -2,14 +2,17 @@ package io.github.pauljamescleary.petstore
 
 import cats.ApplicativeError
 import cats.implicits._
-import com.typesafe.config.ConfigFactory
+import com.typesafe.config.{Config, ConfigFactory}
 import io.circe.Decoder
 import io.circe.config.syntax._
 
 package object config {
+  def loadSafe() : Either[Throwable, Config] =
+    Either.catchNonFatal(ConfigFactory.load())
+
   def loadConfigF[F[_], C : Decoder](implicit ev: ApplicativeError[F, Throwable]) : F[C] =
-    ConfigFactory.load().as[C].leftWiden[Throwable].raiseOrPure[F]
+    loadSafe().flatMap(_.as[C].leftWiden[Throwable]).raiseOrPure[F]
 
   def loadConfigF[F[_], C : Decoder](name: String = "")(implicit ev: ApplicativeError[F, Throwable]) : F[C] =
-    ConfigFactory.load().as[C](name).leftWiden[Throwable].raiseOrPure[F]
+    loadSafe().flatMap(_.as[C](name).leftWiden[Throwable]).raiseOrPure[F]
 }
