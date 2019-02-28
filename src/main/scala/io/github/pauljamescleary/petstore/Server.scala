25bb8bdecd78da8929698bdf54d02d4c1757c410
Igor Rumiha <igorrumiha@gmail.com>
2019-02-28 23:09:38 +0100

    Use different thread pool for connections in dbTransactor.

--- src/main/scala/io/github/pauljamescleary/petstore/Server.scala
+++ src/main/scala/io/github/pauljamescleary/petstore/Server.scala
@@ -12,6 +12,7 @@ import org.http4s.server.{Server => H4Server, Router}
 import org.http4s.server.blaze.BlazeServerBuilder
 import org.http4s.implicits._
 import tsec.passwordhashers.jca.BCrypt
+import doobie.util.ExecutionContexts
 import scala.concurrent.ExecutionContext.Implicits.global
 import io.circe.config.parser
 
@@ -19,7 +20,8 @@ object Server extends IOApp {
   def createServer[F[_] : ContextShift : ConcurrentEffect : Timer]: Resource[F, H4Server[F]] =
     for {
       conf           <- Resource.liftF(parser.decodePathF[F, PetStoreConfig]("petstore"))
-      xa             <- DatabaseConfig.dbTransactor(conf.db, global, global)
+      connEc         <- ExecutionContexts.fixedThreadPool[F](10)
+      xa             <- DatabaseConfig.dbTransactor(conf.db, connEc, global)
       petRepo        =  DoobiePetRepositoryInterpreter[F](xa)
       orderRepo      =  DoobieOrderRepositoryInterpreter[F](xa)
       userRepo       =  DoobieUserRepositoryInterpreter[F](xa)
