d45f199f808ad5655e88cc8aa497d6b0460ef249
Zak <pattersonzak@gmail.com>
2019-05-12 15:12:20 -0500

    Fix tests to easily select between logged in user contects
    pytest fixtures create logged in users, and return lambdas
    that when called log the user in again.

--- functional_test/live_tests/pets_test.py
+++ functional_test/live_tests/pets_test.py
@@ -2,7 +2,17 @@ import pytest
 from hamcrest import assert_that, is_, has_length
 from pet_store_client import PetStoreClient
 
-def test_get_pet(pet_context, pet_store_client):
+example_pet = {
+    "name": "Harry",
+    "category": "Cat",
+    "bio": "I am fuzzy",
+    "status": "Available",
+    "tags": [],
+    "photoUrls": []
+}
+
+
+def test_get_pet(pet_context, customer_context, pet_store_client):
     response = pet_store_client.get_pet(pet_context['id'])
 
     pet = response.json()
@@ -12,7 +22,7 @@ def test_get_pet(pet_context, pet_store_client):
     assert_that(pet['id'], is_(pet_context['id']))
 
 
-def test_list_pets_default_pagination(pet_context, pet_store_client):
+def test_list_pets_default_pagination(pet_context, customer_context, pet_store_client):
     response = pet_store_client.list_pets()
 
     pets = response.json()
@@ -21,7 +31,7 @@ def test_list_pets_default_pagination(pet_context, pet_store_client):
     assert_that(pets[0]['name'], is_('Harry'))
 
 
-def test_list_pets_paginated(pet_context, pet_store_client):
+def test_list_pets_paginated(pet_context, customer_context, pet_store_client):
     response = pet_store_client.list_pets(pageSize=10,offset=0)
     
     pets = response.json()
@@ -30,7 +40,8 @@ def test_list_pets_paginated(pet_context, pet_store_client):
     assert_that(pets[0]['name'], is_('Harry'))
 
 
-def test_find_pets_by_status(pet_context, pet_store_client):
+def test_find_pets_by_status(pet_context, customer_context, pet_store_client):
+
     response = pet_store_client.find_pets_by_status(['Available', 'Pending'])
 
     pets = response.json()
@@ -38,8 +49,9 @@ def test_find_pets_by_status(pet_context, pet_store_client):
 
     assert_that(pets[0]['name'], is_('Harry'))
 
-def test_find_pets_by_tags(pet_context, pet_store_client):
-    # No Pets with "Amphibian" tags exist yet
+def test_find_pets_by_tags(pet_context, customer_context, admin_context, pet_store_client):
+    # No Pets with "Amphibian" tags exist yet(
+    customer_context()
     response = pet_store_client.find_pets_by_tag(['Amphibian'])
     pets = response.json()
     assert_that(pets, has_length(0))
@@ -53,9 +65,12 @@ def test_find_pets_by_tags(pet_context, pet_store_client):
         "tags": ["Green", "Amphibian", "Croaker"],
         "photoUrls": []
     }
+
+    admin_context()
     pet_store_client.create_pet(pet)
 
     # Grab all pets
+    customer_context()
     response = pet_store_client.find_pets_by_tag([''])
     pets = response.json()
     assert_that(pets, has_length(2))
@@ -67,7 +82,7 @@ def test_find_pets_by_tags(pet_context, pet_store_client):
     assert_that(pets, has_length(1))
     assert_that(pets[0]['name'],is_('Nancy'))
 
-def test_update_pet(pet_store_client):
+def test_update_pet(pet_store_client, admin_context):
     pet = {
         "name": "JohnnyUpdate",
         "category": "Cat",
@@ -90,18 +105,24 @@ def test_update_pet(pet_store_client):
         assert_that(updated_pet['bio'], is_('Not so fuzzy'))
     finally:
         if saved_pet:
+            admin_context()
             pet_store_client.delete_pet(saved_pet['id'])
 
 
-def test_update_pet_not_found(pet_store_client):
-    pet = {
-        "id": 99999999,
-        "name": "NotFound",
-        "category": "Cat",
-        "bio": "I am fuzzy",
-        "status": "Available",
-        "tags": [],
-        "photoUrls": []
-    }
+def test_update_pet_not_found(pet_store_client, admin_context):
+    pet = dict(example_pet, **{"id": 9999999})
     response = pet_store_client.update_pet(pet)
     assert_that(response.status_code, is_(404))
+
+def test_customer_cannot_update_pet(pet_store_client, customer_context, admin_context):
+    new_pet = dict(example_pet, **{"name": "test_customer_cannot_update"})
+    admin_context()
+    saved_pet_resp = pet_store_client.create_pet(new_pet)
+    saved_pet = saved_pet_resp.json()
+    customer_context()
+    bad_update = pet_store_client.update_pet(dict(saved_pet, **{"status": "Adopted"}))
+    assert_that(bad_update.status_code, is_(401))
+    admin_context()
+    pet_store_client.delete_pet(saved_pet)
+
+# def test_customer_cannot_delete_pet(pet_store_client, customer_context, admin_context)
