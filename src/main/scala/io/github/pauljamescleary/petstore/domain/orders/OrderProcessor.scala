f1fe0a9c03684d0d108401b81b737d1a76451876
Paul James Cleary <pauljamescleary@gmail.com>
2019-10-23 17:20:50 -0400

    FS2 cleanup

--- src/main/scala/io/github/pauljamescleary/petstore/domain/orders/OrderProcessor.scala
+++ src/main/scala/io/github/pauljamescleary/petstore/domain/orders/OrderProcessor.scala
@@ -12,13 +12,13 @@ import scala.concurrent.duration._
 object OrderProcessor {
 
   def poll[F[_]: Monad: Timer](
-                                interval: FiniteDuration,
-                                queue: OrderQueueAlgebra[F],
+      interval: FiniteDuration,
+      queue: OrderQueueAlgebra[F],
   ): Stream[F, OrderRequest] =
-    Stream.fixedDelay(interval).covary[F].flatMap { _ =>
-      // Fun facts: Option is a Seq, and None is Seq.empty
-      Stream.evalSeq(queue.receive().map(_.toSeq))
-    }
+    for {
+      _ <- Stream.fixedDelay(interval).covary[F]
+      requestStream <- Stream.evalSeq(queue.receive().map(_.toSeq))
+    } yield requestStream
 
   def handleOrder[F[_]: Monad](
       repo: OrderRepositoryAlgebra[F],
@@ -26,20 +26,24 @@ object OrderProcessor {
     _.evalMap { req =>
       req match {
         case Submit(order, _) =>
-          repo.create(order).as(req)
+          repo
+            .create(order)
+            .as(req)
         case Cancel(order, _) =>
-          OptionT.fromOption[F](order.id).semiflatMap(repo.delete).value.as(req)
+          OptionT
+            .fromOption[F](order.id)
+            .semiflatMap(repo.delete)
+            .value
+            .as(req)
       }
     }
 
   def cleanupMessage[F[_]: Monad](queue: OrderQueueAlgebra[F]): Pipe[F, OrderRequest, Unit] =
-    _.evalMap { req =>
-      queue.delete(req.id).void
-    }
+    _.evalMap(req => queue.delete(req.id).void)
 
   def flow[F[_]: Monad: Timer](
-                                repo: OrderRepositoryAlgebra[F],
-                                queue: OrderQueueAlgebra[F],
+      repo: OrderRepositoryAlgebra[F],
+      queue: OrderQueueAlgebra[F],
   ): Stream[F, Unit] =
     poll(5.seconds, queue)
       .through(handleOrder(repo))
